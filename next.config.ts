0c1784c994e14e4642416fdf21f21c08034f3e5e
MUNIR AHMED <lovelyboymunir@gmail.com>
2025-12-05 18:33:58 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://jjikcpu

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'jjikcpuvzemupubhixhc.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
